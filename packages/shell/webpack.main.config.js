cd531a1bef321420a133945429c5b3dc052620a4
zcj <1773821461@qq.com>
2026-01-15 09:18:37 +0800

    feat(shell): 添加TypeScript支持及相关配置
    - 添加ts-loader和typescript依赖
    - 新增tsconfig.json配置文件
    - 更新webpack配置以支持TypeScript文件处理

--- packages/shell/webpack.main.config.js
+++ packages/shell/webpack.main.config.js
@@ -3,7 +3,18 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
   entry: './index.js',
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules|\.webpack)/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
   },
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
--- packages/shell/webpack.renderer.config.js
+++ packages/shell/webpack.renderer.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules|\.webpack)/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
+  },
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
   },
